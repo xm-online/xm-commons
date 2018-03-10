9d68819e6b24cc068029623624e035fad655a6a9
ssenko </dev/null>
2018-03-10 12:46:41 +0200

    Add conflict update handle

--- xm-commons-config/src/main/java/com/icthh/xm/commons/config/client/config/XmConfigAutoConfigration.java
+++ xm-commons-config/src/main/java/com/icthh/xm/commons/config/client/config/XmConfigAutoConfigration.java
@@ -2,6 +2,7 @@ package com.icthh.xm.commons.config.client.config;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
+import com.icthh.xm.commons.config.client.exception.ConflictUpdateConfigException;
 import com.icthh.xm.commons.config.client.repository.TenantConfigRepository;
 import com.icthh.xm.commons.config.client.repository.TenantListRepository;
 import com.icthh.xm.commons.config.client.service.TenantConfigService;
@@ -12,8 +13,14 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.cloud.client.loadbalancer.RestTemplateCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.client.DefaultResponseErrorHandler;
+import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
 
 @Configuration
 @ConditionalOnExpression("${xm-config.enabled} && ${tenant.reject-suspended:true}")
@@ -46,6 +53,16 @@ public class XmConfigAutoConfigration {
         RestTemplate restTemplate = new RestTemplate();
         customizer.customize(restTemplate);
         restTemplate.getMessageConverters().add(0, new StringHttpMessageConverter(UTF_8));
+        restTemplate.setErrorHandler(new DefaultResponseErrorHandler() {
+            @Override
+            public void handleError(ClientHttpResponse response) throws IOException {
+                if (response.getStatusCode() == HttpStatus.CONFLICT) {
+                    throw new ConflictUpdateConfigException();
+                } else {
+                    super.handleError(response);
+                }
+            }
+        });
         return restTemplate;
     }
 
