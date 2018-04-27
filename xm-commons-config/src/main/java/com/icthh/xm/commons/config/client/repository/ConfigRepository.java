efbb234c429c7bf029e27dc507b4b2d62e1117b4
emonastyrev <emonastyrev@jbs.com.ua>
2018-04-27 16:22:10 +0300

    fix get config

--- xm-commons-config/src/main/java/com/icthh/xm/commons/config/client/repository/ConfigRepository.java
+++ xm-commons-config/src/main/java/com/icthh/xm/commons/config/client/repository/ConfigRepository.java
@@ -3,7 +3,6 @@ package com.icthh.xm.commons.config.client.repository;
 import static com.icthh.xm.commons.config.client.utils.RequestUtils.createSimpleHeaders;
 
 import com.icthh.xm.commons.config.client.config.XmConfigProperties;
-import com.icthh.xm.commons.config.domain.ConfigurationFile;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.ParameterizedTypeReference;
@@ -11,9 +10,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -28,14 +25,13 @@ public class ConfigRepository {
     public Map<String, String> getMap() {
         Map<String, String> configuration = configMap;
         if (configuration == null) {
-            configMap = readFromConfigService().stream()
-                .collect(Collectors.toMap(e -> e.getPath(), e -> e.getContent()));
+            configMap = readFromConfigService();
         }
         return configMap;
     }
 
-    private List<ConfigurationFile> readFromConfigService() {
-        ParameterizedTypeReference<List<ConfigurationFile>> typeRef = new ParameterizedTypeReference<List<ConfigurationFile>>() {
+    private Map<String, String> readFromConfigService() {
+        ParameterizedTypeReference<Map<String, String>> typeRef = new ParameterizedTypeReference<Map<String, String>>() {
         };
         HttpEntity<String> entity = new HttpEntity<>(createSimpleHeaders());
         return restTemplate.exchange(getServiceConfigUrl(), HttpMethod.GET, entity, typeRef).getBody();
