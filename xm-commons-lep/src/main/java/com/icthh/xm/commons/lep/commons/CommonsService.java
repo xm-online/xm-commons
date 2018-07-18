58def65af9513a37435335429ad19dcf9b0639d1
ssenko <sergeysenja1992@gmail.com>
2018-07-18 16:51:09 +0300

    lep commons

---
+++ xm-commons-lep/src/main/java/com/icthh/xm/commons/lep/commons/CommonsService.java
@@ -0,0 +1,19 @@
+package com.icthh.xm.commons.lep.commons;
+
+import com.icthh.xm.commons.lep.LogicExtensionPoint;
+import com.icthh.xm.commons.lep.spring.LepService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.NotImplementedException;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@LepService
+public class CommonsService {
+
+    @LogicExtensionPoint(value = "Commons", resolver = CommonsLepResolver.class)
+    public Object execute(String group, String name, Object args) {
+        throw new NotImplementedException("Commons in package:" + group + " with name: Commons$$" + name + "$$around.groovy not found");
+    }
+
+}
