c336779f7c76792de796e3a370b33870a2e922ad
vkirichenko <vitaliy.v.kirichenko@gmail.com>
2018-12-20 17:09:41 +0200

    db migration commons

--- xm-commons-migration-db/src/main/java/com/icthh/xm/commons/migration/db/tenant/SchemaChangeResolver.java
+++ xm-commons-migration-db/src/main/java/com/icthh/xm/commons/migration/db/tenant/SchemaChangeResolver.java
@@ -2,11 +2,11 @@ package com.icthh.xm.commons.migration.db.tenant;
 
 import static com.icthh.xm.commons.migration.db.Constants.DB_SCHEMA_SUFFIX;
 import static com.icthh.xm.commons.migration.db.Constants.JPA_VENDOR;
+import static org.apache.commons.lang3.StringUtils.EMPTY;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -18,7 +18,7 @@ import java.util.Map;
 @Component
 public class SchemaChangeResolver {
 
-    private static final String DEFAULT_COMMAND = "USE ";
+    private static final String DEFAULT_COMMAND = "USE %s";
     private static final Map<String, String> DB_COMMANDS = new HashMap<>();
 
     private String dbSchemaChangeCommand;
@@ -40,14 +40,11 @@ public class SchemaChangeResolver {
     }
 
     private void initDbCommands(Environment env) {
-        String schemaSuffix = env.getProperty(DB_SCHEMA_SUFFIX);
-        String schema = "%s";
+        String schemaSuffix = env.getProperty(DB_SCHEMA_SUFFIX, EMPTY);
 
-        schema += StringUtils.isEmpty(schemaSuffix) ? "" : schemaSuffix;
-
-        DB_COMMANDS.put("POSTGRESQL", "SET search_path TO " + schema);
-        DB_COMMANDS.put("ORACLE", "ALTER SESSION SET CURRENT_SCHEMA = " + schema);
-        DB_COMMANDS.put("H2", DEFAULT_COMMAND + schema);
+        DB_COMMANDS.put("POSTGRESQL", "SET search_path TO %s" + schemaSuffix);
+        DB_COMMANDS.put("ORACLE", "ALTER SESSION SET CURRENT_SCHEMA = %s" + schemaSuffix);
+        DB_COMMANDS.put("H2", DEFAULT_COMMAND + schemaSuffix);
     }
 
 }
