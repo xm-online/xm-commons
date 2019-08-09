181ca57b17b0d1d7c02b66eaa24c8f7f40767e36
Anatoliy Medvedchuck <anatoliy.medvedchuk@jevera.software>
2019-08-09 12:18:38 +0300

    Implement feature to init only selected tenants

--- xm-commons-config/src/main/java/com/icthh/xm/commons/config/client/repository/TenantListRepository.java
+++ xm-commons-config/src/main/java/com/icthh/xm/commons/config/client/repository/TenantListRepository.java
@@ -2,7 +2,6 @@ package com.icthh.xm.commons.config.client.repository;
 
 import static com.fasterxml.jackson.databind.type.TypeFactory.defaultInstance;
 import static com.icthh.xm.commons.config.client.utils.RequestUtils.createAuthHeaders;
-import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
 import static java.util.Collections.unmodifiableSet;
 
@@ -13,7 +12,6 @@ import com.icthh.xm.commons.config.client.api.RefreshableConfiguration;
 import com.icthh.xm.commons.config.client.config.XmConfigProperties;
 import com.icthh.xm.commons.config.domain.Configuration;
 import com.icthh.xm.commons.config.domain.TenantState;
-import java.util.Collections;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections.CollectionUtils;
@@ -25,7 +23,9 @@ import org.springframework.web.client.RestTemplate;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 @Slf4j
@@ -33,7 +33,7 @@ public class TenantListRepository implements RefreshableConfiguration {
 
     public static final String TENANTS_LIST_CONFIG_KEY = "/config/tenants/tenants-list.json";
 
-    public static final String URL = "/api/tenants/";
+    private static final String URL = "/api/tenants/";
 
     private static final String ERROR = "Tenant list not found. Maybe xm-config not running.";
 
@@ -47,16 +47,19 @@ public class TenantListRepository implements RefreshableConfiguration {
 
     private final String xmConfigUrl;
 
+    private final Set<String> includeTenants;
+
     private volatile Set<TenantState> tenants = new HashSet<>();
     private volatile Set<String> suspendedTenants = new HashSet<>();
 
     public TenantListRepository(RestTemplate restTemplate,
                                 CommonConfigRepository commonConfigRepository,
                                 String applicationName,
-                                XmConfigProperties applicationProperties) {
+                                XmConfigProperties xmConfigProperties) {
         this.restTemplate = restTemplate;
         this.applicationName = applicationName;
-        this.xmConfigUrl = applicationProperties.getXmConfigUrl() + URL;
+        this.xmConfigUrl = xmConfigProperties.getXmConfigUrl() + URL;
+        this.includeTenants = xmConfigProperties.getIncludeTenantLowercase();
 
         Configuration configuration = commonConfigRepository.getConfig(null, singletonList(TENANTS_LIST_CONFIG_KEY)).
             get(TENANTS_LIST_CONFIG_KEY);
@@ -79,17 +82,20 @@ public class TenantListRepository implements RefreshableConfiguration {
         return unmodifiableSet(suspendedTenants);
     }
 
+    @SuppressWarnings("unused")
     public void addTenant(String tenantName) {
         HttpEntity<String> entity = new HttpEntity<>(tenantName.toLowerCase(), createAuthHeaders());
         restTemplate.postForEntity(xmConfigUrl + applicationName, entity, Void.class);
     }
 
+    @SuppressWarnings("unused")
     public void deleteTenant(String tenantName) {
         HttpEntity<String> entity = new HttpEntity<>(createAuthHeaders());
         restTemplate.exchange(xmConfigUrl + applicationName + "/" + tenantName.toLowerCase(),
                               HttpMethod.DELETE, entity, Void.class);
     }
 
+    @SuppressWarnings("unused")
     public void updateTenant(String tenantName, String state) {
         HttpEntity<String> entity = new HttpEntity<>(state, createAuthHeaders());
         restTemplate.exchange(xmConfigUrl + applicationName + "/" + tenantName.toLowerCase(),
@@ -106,16 +112,49 @@ public class TenantListRepository implements RefreshableConfiguration {
 
         assertExistsTenantsListConfig(config);
 
-        CollectionType setType = defaultInstance().constructCollectionType(HashSet.class, TenantState.class);
-        MapType type = defaultInstance().constructMapType(HashMap.class, defaultInstance().constructType(String.class), setType);
-        Map<String, Set<TenantState>> tenantsByServiceMap = objectMapper.readValue(config, type);
-        Set<TenantState> tenantKeys = tenantsByServiceMap.get(applicationName);
+        Set<TenantState> tenantKeys = parseTenantStates(config, applicationName, objectMapper);
 
         assertExistTenants(tenantKeys);
 
-        this.tenants = tenantKeys;
-        this.suspendedTenants = tenantKeys.stream().filter(tenant -> SUSPENDED_STATE.equals(tenant.getState()))
-            .map(TenantState::getName).collect(Collectors.toSet());
+        if (!includeTenants.isEmpty()) {
+            log.warn("Tenant list was overridden by property 'xm-config.include-tenants' to: {}", includeTenants);
+        }
+
+        this.tenants = tenantKeys.stream()
+                                 .filter(isIncluded(includeTenants))
+                                 .collect(Collectors.toSet());
+
+        this.suspendedTenants = tenantKeys.stream()
+                                          .filter(isSuspended())
+                                          .map(TenantState::getName)
+                                          .collect(Collectors.toSet());
+    }
+
+    public static Predicate<TenantState> isIncluded(Set<String> includedTenants) {
+        return tenantState -> includedTenants == null
+                              || includedTenants.isEmpty()
+                              || includedTenants.contains(tenantState.getName());
+    }
+
+    public static Predicate<TenantState> isSuspended() {
+        return tenantState -> SUSPENDED_STATE.equals(tenantState.getState());
+    }
+
+    private static Set<TenantState> parseTenantStates(String tenantListJson, String appName, ObjectMapper mapper) throws java.io.IOException {
+        return parseTenantStates(tenantListJson, mapper).get(appName);
+    }
+
+    /**
+     * Parsses tenant-list.json to map by application and set of @{@link TenantState}
+     * @param tenantListJson source tenant list
+     * @param mapper ObjectMapper
+     * @return Map with application name as a key and set of tenant states as a value.
+     */
+    public static Map<String, Set<TenantState>> parseTenantStates(String tenantListJson, ObjectMapper mapper) throws java.io.IOException {
+        CollectionType setType = defaultInstance().constructCollectionType(HashSet.class, TenantState.class);
+        MapType type = defaultInstance().constructMapType(HashMap.class, defaultInstance().constructType(String.class), setType);
+        Map<String, Set<TenantState>> map = mapper.readValue(tenantListJson, type);
+        return Optional.ofNullable(map).orElse(new HashMap<>());
     }
 
     private void assertExistTenants(Set<TenantState> tenantKeys) {
---
+++ xm-commons-config/src/test/java/com/icthh/xm/commons/config/client/repository/TenantListRepositoryUnitTest.java
@@ -0,0 +1,157 @@
+package com.icthh.xm.commons.config.client.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyCollection;
+import static org.mockito.Mockito.when;
+
+import com.icthh.xm.commons.config.client.config.XmConfigProperties;
+import com.icthh.xm.commons.config.domain.Configuration;
+import lombok.SneakyThrows;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.web.client.RestTemplate;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TenantListRepositoryUnitTest {
+
+    private TenantListRepository tenantListRepository;
+
+    @Mock
+    private RestTemplate restTemplate;
+    @Mock
+    private CommonConfigRepository commonConfigRepository;
+
+    private String applicationName = "entity";
+    @Spy
+    private XmConfigProperties xmConfigProperties;
+
+    @Rule
+    public ExpectedException expectedEx = ExpectedException.none();
+
+    @Before
+    @SneakyThrows
+    public void init() {
+
+        MockitoAnnotations.initMocks(this);
+
+        when(xmConfigProperties.getXmConfigUrl()).thenReturn("xm-config-url");
+
+        Configuration configuration = new Configuration();
+        configuration.setPath("<mock>");
+        configuration.setContent(readFile("/config/tenants/tenants-list.json"));
+
+        when(commonConfigRepository.getConfig(any(), anyCollection())).thenReturn(Collections.singletonMap(
+            TenantListRepository.TENANTS_LIST_CONFIG_KEY, configuration));
+
+        tenantListRepository = new TenantListRepository(restTemplate,
+                                                        commonConfigRepository,
+                                                        applicationName,
+                                                        xmConfigProperties);
+
+    }
+
+    @Test
+    public void shouldFailTenantListNotFound() {
+
+        expectedEx.expect(IllegalStateException.class);
+        expectedEx.expectMessage("Tenant list not found. Maybe xm-config not running.");
+
+        when(commonConfigRepository.getConfig(any(), anyCollection())).thenReturn(Collections.singletonMap(
+            TenantListRepository.TENANTS_LIST_CONFIG_KEY, null));
+
+        new TenantListRepository(restTemplate, commonConfigRepository, applicationName, xmConfigProperties);
+    }
+
+    @Test
+    public void shouldFailTenantListForAppNameisEmpty() {
+
+        expectedEx.expect(IllegalStateException.class);
+        expectedEx.expectMessage("Tenant list for wrong-app empty. Check tenants-list.json.");
+
+        new TenantListRepository(restTemplate, commonConfigRepository, "wrong-app", xmConfigProperties);
+    }
+
+    @Test
+    public void testGetTenants() {
+
+        Set<String> tenants = tenantListRepository.getTenants();
+
+        assertNotNull(tenants);
+        assertEquals(3, tenants.size());
+        assertThat(tenants).containsExactlyInAnyOrder("xm", "demo", "susp");
+
+        Set<String> suspendedTenants = tenantListRepository.getSuspendedTenants();
+        assertNotNull(suspendedTenants);
+        assertEquals(1, suspendedTenants.size());
+        assertThat(suspendedTenants).containsExactlyInAnyOrder("susp");
+    }
+
+    @Test
+    public void testRefreshTenants() {
+
+        Set<String> tenants = tenantListRepository.getTenants();
+
+        assertNotNull(tenants);
+        assertEquals(3, tenants.size());
+        assertThat(tenants).containsExactlyInAnyOrder("xm", "demo", "susp");
+
+        tenantListRepository.onRefresh("/config/tenants/tenants-list.json",
+                                       readFile("/config/tenants/tenants-list-updated.json"));
+
+        tenants = tenantListRepository.getTenants();
+
+        assertNotNull(tenants);
+        assertEquals(4, tenants.size());
+        assertThat(tenants).containsExactlyInAnyOrder("xm", "demo", "susp", "added");
+
+    }
+
+    @Test
+    public void testGetTenantsIncluded() {
+
+        Set<String> tenants = tenantListRepository.getTenants();
+
+        assertNotNull(tenants);
+        assertEquals(3, tenants.size());
+
+        Set<String> included = new HashSet<>();
+        included.add("xm");
+        included.add("DEMO");
+
+        when(xmConfigProperties.getIncludeTenants()).thenReturn(included);
+
+        tenantListRepository = new TenantListRepository(restTemplate,
+                                                        commonConfigRepository,
+                                                        applicationName,
+                                                        xmConfigProperties);
+
+        tenants = tenantListRepository.getTenants();
+
+        assertNotNull(tenants);
+        assertEquals(2, tenants.size());
+        assertThat(tenants).containsExactlyInAnyOrder("xm","demo");
+
+    }
+
+    @SneakyThrows
+    private String readFile(String path) {
+        return new String(Files.readAllBytes(Paths.get(TenantListRepository.class.getResource(path).toURI())));
+    }
+
+}
