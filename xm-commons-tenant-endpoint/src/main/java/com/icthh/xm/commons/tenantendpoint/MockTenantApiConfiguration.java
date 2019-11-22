fae78d34eb94a7c9acaefb207c7bd94aa3a1fa37
Dmittry Tsarenko <dimatsarenko89@gmail.com>
2019-11-22 15:43:41 +0200

    add mockTenantApiConfiguration

--- xm-commons-tenant-endpoint/src/main/java/com/icthh/xm/commons/tenantendpoint/MockTenantApiConfiguration.java
+++ xm-commons-tenant-endpoint/src/main/java/com/icthh/xm/commons/tenantendpoint/MockTenantApiConfiguration.java
@@ -2,6 +2,7 @@ package com.icthh.xm.commons.tenantendpoint;
 
 import com.icthh.xm.commons.gen.api.TenantsApiDelegate;
 import com.icthh.xm.commons.gen.model.Tenant;
+import org.apache.commons.lang3.NotImplementedException;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,33 +13,35 @@ import java.util.List;
 @Configuration
 public class MockTenantApiConfiguration  {
 
+    private static final String ERROR_MESSAGE = "Tenant api controller is not implemented for this microservice";
+
     @ConditionalOnMissingBean(TenantsApiDelegate.class)
     @Bean
     public TenantsApiDelegate tenantsApiDelegate() {
         return new TenantsApiDelegate() {
             @Override
             public ResponseEntity<Void> addTenant(Tenant body) {
-                return null;
+                throw new NotImplementedException(ERROR_MESSAGE);
             }
 
             @Override
             public ResponseEntity<Void> deleteTenant(String tenantKey) {
-                return null;
+                throw new NotImplementedException(ERROR_MESSAGE);
             }
 
             @Override
             public ResponseEntity<List<Tenant>> getAllTenantInfo() {
-                return null;
+                throw new NotImplementedException(ERROR_MESSAGE);
             }
 
             @Override
             public ResponseEntity<Tenant> getTenant(String tenantKey) {
-                return null;
+                throw new NotImplementedException(ERROR_MESSAGE);
             }
 
             @Override
             public ResponseEntity<Void> manageTenant(String tenantKey, String body) {
-                return null;
+                throw new NotImplementedException(ERROR_MESSAGE);
             }
         };
     }
