95fe3f6868efaec8f33d46313652796f27f718f8
vkirichenko <vitaliy.v.kirichenko@gmail.com>
2019-12-02 12:34:14 +0200

    implementation of xm-commons-topics

---
+++ xm-commons-messaging/src/main/java/com/icthh/xm/commons/messaging/communication/Receiver.java
@@ -0,0 +1,22 @@
+package com.icthh.xm.commons.messaging.communication;
+
+import lombok.Data;
+
+/**
+ * Receivers of the communication message.
+ */
+@Data
+public class Receiver {
+
+    private String id;
+    private String appUserId;
+    private String email;
+    private String ip;
+    private String name;
+    private String phoneNumber;
+    private String type;
+    private String atType;
+    private String atBaseType;
+    private String atSchemaLocation;
+}
+
---
+++ xm-commons-messaging/src/main/java/com/icthh/xm/commons/messaging/communication/Sender.java
@@ -0,0 +1,19 @@
+package com.icthh.xm.commons.messaging.communication;
+
+import lombok.Data;
+
+/**
+ * Sender of the communication message.
+ */
+@Data
+public class Sender {
+
+    private String id;
+    private String email;
+    private String name;
+    private String phoneNumber;
+    private String atType;
+    private String atBaseType;
+    private String atSchemaLocation;
+}
+
