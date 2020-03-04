8aa22504a79aac14c35958d7a7fefed19ddd8185
yevheniiMuzyka <email>
2020-03-04 10:09:22 +0200

    add filter for reflections

--- xm-commons-permission/src/main/java/com/icthh/xm/commons/permission/config/ReflectionConfig.java
+++ xm-commons-permission/src/main/java/com/icthh/xm/commons/permission/config/ReflectionConfig.java
@@ -4,6 +4,7 @@ import org.reflections.Reflections;
 import org.reflections.scanners.MethodAnnotationsScanner;
 import org.reflections.util.ClasspathHelper;
 import org.reflections.util.ConfigurationBuilder;
+import org.reflections.util.FilterBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -23,6 +24,7 @@ public class ReflectionConfig {
     public Reflections reflections() {
         return new Reflections(new ConfigurationBuilder()
                                     .setUrls(ClasspathHelper.forPackage(scanPackage.trim()))
-                                    .setScanners(new MethodAnnotationsScanner()));
+                                    .setScanners(new MethodAnnotationsScanner())
+                                    .filterInputsBy(new FilterBuilder().include(".*class")));
     }
 }
