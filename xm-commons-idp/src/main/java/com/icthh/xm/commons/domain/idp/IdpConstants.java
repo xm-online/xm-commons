ec8784a8bdca56c85ced1cde190ab07b7c8dda6d
olye0216 <oleksandr.yershov@netcracker.com>
2021-02-09 16:28:07 +0200

    #aaukrp update IDP_PUBLIC_SETTINGS_CONFIG_PATH_PATTERN path

--- xm-commons-idp/src/main/java/com/icthh/xm/commons/domain/idp/IdpConstants.java
+++ xm-commons-idp/src/main/java/com/icthh/xm/commons/domain/idp/IdpConstants.java
@@ -2,7 +2,7 @@ package com.icthh.xm.commons.domain.idp;
 
 public class IdpConstants {
 
-    public static final String IDP_PUBLIC_SETTINGS_CONFIG_PATH_PATTERN = "/config/tenants/{tenant}/idp-config-public.yml";
+    public static final String IDP_PUBLIC_SETTINGS_CONFIG_PATH_PATTERN = "/config/tenants/{tenant}/webapp/public/idp-config-public.yml";
     public static final String IDP_PRIVATE_SETTINGS_CONFIG_PATH_PATTERN = "/config/tenants/{tenant}/idp-config-private.yml";
     public static final String PUBLIC_JWKS_CONFIG_PATH_PATTERN = "/config/tenants/{tenant}/config/idp/clients/";
     public static final String JWKS_FILE_NAME_PATTERN = "{idpClientKey}-jwks-cache.json";
