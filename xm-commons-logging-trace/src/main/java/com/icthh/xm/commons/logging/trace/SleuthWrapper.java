af81779ccec5a29567e19cb83633effe9f54283b
Valerii Bilenko <valerii.bilenko@jevera.software>
2022-06-21 13:59:45 +0300

    not required beans

--- xm-commons-logging-trace/src/main/java/com/icthh/xm/commons/logging/trace/SleuthWrapper.java
+++ xm-commons-logging-trace/src/main/java/com/icthh/xm/commons/logging/trace/SleuthWrapper.java
@@ -3,23 +3,36 @@ package com.icthh.xm.commons.logging.trace;
 import brave.Span;
 import brave.Tracer;
 import brave.kafka.clients.KafkaTracing;
-import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.sleuth.instrument.messaging.TracingChannelInterceptor;
 import org.springframework.messaging.Message;
 import org.springframework.stereotype.Component;
 
 @Component
-@AllArgsConstructor
 @Slf4j
 public class SleuthWrapper {
 
     private static final String SPAN_NAME_FROM_MESSAGE = "on-message";
 
     private final Tracer tracer;
-    private final KafkaTracing kafkaTracing;
-    private final TracingChannelInterceptor tracingChannelInterceptor;
+    private KafkaTracing kafkaTracing;
+    private TracingChannelInterceptor tracingChannelInterceptor;
+
+    public SleuthWrapper(Tracer tracer) {
+        this.tracer = tracer;
+    }
+
+    @Autowired(required = false)
+    public void setKafkaTracing(KafkaTracing kafkaTracing) {
+        this.kafkaTracing = kafkaTracing;
+    }
+
+    @Autowired(required = false)
+    public void setTracingChannelInterceptor(TracingChannelInterceptor tracingChannelInterceptor) {
+        this.tracingChannelInterceptor = tracingChannelInterceptor;
+    }
 
     public void runWithSleuth(ConsumerRecord<?, ?> record, Runnable codeToRun) {
         Span kafkaSpan = kafkaTracing.nextSpan(record).name(SPAN_NAME_FROM_MESSAGE).start();
