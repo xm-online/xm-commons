cd7ca1ef842d2a3b12a974ee0ffda1549138d866
Anatoliy Medvedchuk <anatoliy.medvedchuk@jevera.software>
2022-08-01 12:58:52 +0300

    fix 401 response body issue

--- xm-commons-security/src/main/java/com/icthh/xm/commons/security/spring/config/UnauthorizedEntryPoint.java
+++ xm-commons-security/src/main/java/com/icthh/xm/commons/security/spring/config/UnauthorizedEntryPoint.java
@@ -1,16 +1,32 @@
 package com.icthh.xm.commons.security.spring.config;
 
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.AuthenticationEntryPoint;
-
-import javax.servlet.ServletException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
 
 public final class UnauthorizedEntryPoint implements AuthenticationEntryPoint {
 	public void commence(HttpServletRequest request, HttpServletResponse response,
 						 AuthenticationException authException) throws IOException {
-		response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+		try (var writer = response.getWriter()) {
+			writer.print(new ObjectMapper().writeValueAsString(new ErrorVM(
+					"unauthorized",
+					"Full authentication is required to access this resource"
+			)));
+			response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+		}
+	}
+
+	@Getter
+	@RequiredArgsConstructor
+	public static class ErrorVM {
+		private final String error;
+		private final String error_description;
 	}
 }
