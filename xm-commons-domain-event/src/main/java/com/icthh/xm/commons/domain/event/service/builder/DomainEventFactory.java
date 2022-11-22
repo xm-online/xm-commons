d5d57eae4f7d1e3c8158751d08aaba341b1961c4
Valerii Bilenko <valerii.bilenko@jevera.software>
2022-11-22 17:15:43 +0200

    pull request updates

--- xm-commons-domain-event/src/main/java/com/icthh/xm/commons/domain/event/service/builder/DomainEventFactory.java
+++ xm-commons-domain-event/src/main/java/com/icthh/xm/commons/domain/event/service/builder/DomainEventFactory.java
@@ -3,75 +3,75 @@ package com.icthh.xm.commons.domain.event.service.builder;
 import com.icthh.xm.commons.domain.event.domain.enums.DefaultDomainEventOperation;
 import com.icthh.xm.commons.domain.event.service.dto.DomainEvent;
 import com.icthh.xm.commons.domain.event.service.dto.DomainEventPayload;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DomainEventFactory {
-    private final DomainEventBuilder defaultDomainEventBuilder;
-    private final DomainEventBuilder transactionalDomainEventBuilder;
+    private final DomainEventBuilder domainEventBuilder;
+    private DomainEventBuilder transactionalDomainEventBuilder;
 
+    @Autowired
     public DomainEventFactory(@Qualifier("defaultDomainEventBuilder") DomainEventBuilder defaultDomainEventBuilder,
                               @Qualifier("txDomainEventBuilder") DomainEventBuilder transactionalDomainEventBuilder) {
-        this.defaultDomainEventBuilder = defaultDomainEventBuilder;
         this.transactionalDomainEventBuilder = transactionalDomainEventBuilder;
+        this.domainEventBuilder = defaultDomainEventBuilder;
     }
 
-    public Factory withoutTransaction() {
-        return new Factory(defaultDomainEventBuilder);
+    private DomainEventFactory(DomainEventBuilder domainEventBuilder) {
+        this.domainEventBuilder = domainEventBuilder;
     }
 
-    public Factory withTransaction() {
-        return new Factory(transactionalDomainEventBuilder);
+    /**
+     * With this configuration factory will retrieve current transaction id from database
+     * and insert it into txId field. Additional database query will be executed.
+     *
+     * @return factory that will set current transaction id to txId field.
+     */
+    public DomainEventFactory withTransaction() {
+        return new DomainEventFactory(transactionalDomainEventBuilder);
     }
 
-    public static class Factory {
-        private final DomainEventBuilder domainEventBuilder;
+    public DomainEvent.DomainEventBuilder builder() {
+        return domainEventBuilder.getPrefilledBuilder();
+    }
 
-        public Factory(DomainEventBuilder domainEventBuilder) {
-            this.domainEventBuilder = domainEventBuilder;
-        }
+    public DomainEvent build() {
+        return domainEventBuilder.getPrefilledBuilder().build();
+    }
 
-        public DomainEvent.DomainEventBuilder builder() {
-            return domainEventBuilder.getPrefilledBuilder();
-        }
+    public DomainEvent build(DomainEventPayload payload) {
+        return builder()
+            .payload(payload)
+            .build();
+    }
 
-        public DomainEvent build() {
-            return domainEventBuilder.getPrefilledBuilder().build();
-        }
+    public DomainEvent build(DefaultDomainEventOperation operation) {
+        return builder()
+            .operation(operation == null ? null : operation.toString())
+            .build();
+    }
 
-        public DomainEvent build(DomainEventPayload payload) {
-            return builder()
-                .payload(payload)
-                .build();
-        }
+    public DomainEvent build(DefaultDomainEventOperation operation,
+                             String aggregateId,
+                             String aggregateType) {
+        return builder()
+            .aggregateId(aggregateId)
+            .aggregateType(aggregateType)
+            .operation(operation == null ? null : operation.toString())
+            .build();
+    }
 
-        public DomainEvent build(DefaultDomainEventOperation operation) {
-            return builder()
-                .operation(operation == null ? null : operation.toString())
-                .build();
-        }
-
-        public DomainEvent build(DefaultDomainEventOperation operation,
-                                 String aggregateId,
-                                 String aggregateType) {
-            return builder()
-                .aggregateId(aggregateId)
-                .aggregateType(aggregateType)
-                .operation(operation == null ? null : operation.toString())
-                .build();
-        }
-
-        public DomainEvent build(DefaultDomainEventOperation operation,
-                                 String aggregateId,
-                                 String aggregateType,
-                                 DomainEventPayload payload) {
-            return builder()
-                .aggregateId(aggregateId)
-                .aggregateType(aggregateType)
-                .operation(operation == null ? null : operation.toString())
-                .payload(payload)
-                .build();
-        }
+    public DomainEvent build(DefaultDomainEventOperation operation,
+                             String aggregateId,
+                             String aggregateType,
+                             DomainEventPayload payload) {
+        return builder()
+            .aggregateId(aggregateId)
+            .aggregateType(aggregateType)
+            .operation(operation == null ? null : operation.toString())
+            .payload(payload)
+            .build();
     }
 }
