0d7815ca2ed4d1364d63e67453559f3677640bf3
bvolokhenko <bohdan.volokhonenko@jevera.software>
2022-12-16 11:00:17 +0200

    Encapsulated sending communication message

---
+++ xm-commons-messaging/src/main/java/com/icthh/xm/commons/messaging/communication/CommunicationConstants.java
@@ -0,0 +1,7 @@
+package com.icthh.xm.commons.messaging.communication;
+
+public class CommunicationConstants {
+    public static final String TEMPLATE_MODEL  = "templateModel";
+    public static final String LANGUAGE = "language";
+    public static final String TEMPLATE_NAME = "templateName";
+}
