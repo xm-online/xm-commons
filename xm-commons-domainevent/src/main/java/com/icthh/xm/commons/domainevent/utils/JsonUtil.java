dffac2ba7bec3a5ae8009070a5a13bc5e088bc58
emurenko <murenko-ye@mc.gcf>
2023-01-17 12:32:07 +0200

    optimization of JsonUtil parser

--- xm-commons-domainevent/src/main/java/com/icthh/xm/commons/domainevent/utils/JsonUtil.java
+++ xm-commons-domainevent/src/main/java/com/icthh/xm/commons/domainevent/utils/JsonUtil.java
@@ -17,57 +17,61 @@ public class JsonUtil {
     private static final String ID_FIELD_NAME = "id";
     private static final String TYPE_KEY_FIELD_NAME = "typeKey";
 
-    public static Pair<String, String> extractIdAndTypeKey(JsonFactory jsonFactory, String json) {
-        String id = null;
-        String typeKey = null;
+    private static final int ID = 0;
+    private static final int TYPE_KEY = 1;
 
+    public static Pair<String, String> extractIdAndTypeKey(JsonFactory jsonFactory, String json) {
+        String[] values = new String[2];
         try (JsonParser jParser = jsonFactory.createParser(json)) {
             while (jParser.nextToken() != JsonToken.END_OBJECT) {
                 String fieldName = jParser.getCurrentName();
+
+                if (fieldName == null) {
+                    continue;
+                }
+
                 if (ID_FIELD_NAME.equals(fieldName)) {
                     jParser.nextToken();
-                    id = jParser.getText();
+                    values[ID] = jParser.getText();
+                    continue;
                 }
 
                 if (TYPE_KEY_FIELD_NAME.equals(fieldName)) {
                     jParser.nextToken();
-                    typeKey = jParser.getText();
+                    values[TYPE_KEY] = jParser.getText();
+                    continue;
                 }
 
                 if ("xmEntity".equals(fieldName)) {
-                    Pair<String, String> innerPair = extractFromInnerLevel(jParser, id, typeKey);
-                    id = innerPair.getKey();
-                    typeKey = innerPair.getValue();
+                    extractFromInnerLevel(jParser, values);
                 }
 
                 if ("data".equals(fieldName)) {
-                    Pair<String, String> innerPair = extractFromInnerLevel(jParser, id, typeKey);
-                    id = innerPair.getKey();
-                    typeKey = innerPair.getValue();
+                    extractFromInnerLevel(jParser, values);
                 }
             }
         } catch (IOException e) {
             log.trace(e.getMessage(), e);
         }
-        return Pair.of(id, typeKey);
+        return Pair.of(values[ID], values[TYPE_KEY]);
     }
 
     @SneakyThrows
-    private Pair<String, String> extractFromInnerLevel(JsonParser jParser, String currentId, String currentTypeKey) {
+    private void extractFromInnerLevel(JsonParser jParser, String[] result) {
         jParser.nextToken();
         while (jParser.nextToken() != JsonToken.END_OBJECT) {
             String innerFieldName = jParser.getCurrentName();
-            if (currentId == null && "id".equals(innerFieldName)) {
+
+            if (result[ID] == null && ID_FIELD_NAME.equals(innerFieldName)) {
                 jParser.nextToken();
-                currentId = jParser.getText();
+                result[ID] = jParser.getText();
+                continue;
             }
 
-            if (currentTypeKey == null && "typeKey".equals(innerFieldName)) {
+            if (result[TYPE_KEY] == null && TYPE_KEY_FIELD_NAME.equals(innerFieldName)) {
                 jParser.nextToken();
-                currentTypeKey = jParser.getText();
+                result[TYPE_KEY] = jParser.getText();
             }
         }
-
-        return Pair.of(currentId, currentTypeKey);
     }
 }
