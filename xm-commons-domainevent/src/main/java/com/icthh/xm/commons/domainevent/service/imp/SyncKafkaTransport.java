3969172a93bd5f80f873e443c25029f3fba5fd8a
emurenko <murenko-ye@mc.gcf>
2023-01-27 18:24:36 +0200

    adding implementation info

--- xm-commons-domainevent/src/main/java/com/icthh/xm/commons/domainevent/service/imp/SyncKafkaTransport.java
+++ xm-commons-domainevent/src/main/java/com/icthh/xm/commons/domainevent/service/imp/SyncKafkaTransport.java
@@ -13,6 +13,29 @@ import org.springframework.transaction.support.TransactionSynchronizationManager
 
 import java.util.function.Consumer;
 
+/**
+ *  Example of KafkaTransactionalConfig implementation. Should be implemented in main microservice to avoid:
+ *  Caused by: java.lang.IllegalAccessError: class com.icthh.xm.commons.domainevent.config.KafkaTransactionalConfig$1$$EnhancerBySpringCGLIB$$79a220b3
+ *  cannot access its superclass com.icthh.xm.commons.domainevent.config.KafkaTransactionalConfig$1
+ *  (com.icthh.xm.commons.domainevent.config.KafkaTransactionalConfig$1$$EnhancerBySpringCGLIB$$79a220b3 is in unnamed module
+ *  of loader org.springframework.boot.devtools.restart.classloader.RestartClassLoader @24163a8c;
+ *  com.icthh.xm.commons.domainevent.config.KafkaTransactionalConfig$1 is in unnamed module of loader 'app')
+ *
+ *    public KafkaTransactionalConfig(ConfigurableListableBeanFactory factory) {
+ *         factory.registerScope("transaction", new SimpleTransactionScope());
+ *     }
+ *
+ *     @Bean
+ *     default KafkaTransactionSynchronizationAdapterService kafkaTransactionSynchronizationAdapterService(ApplicationContext context) {
+ *         return new KafkaTransactionSynchronizationAdapterService() {
+ *             @Override
+ *             public KafkaTransactionSynchronizationAdapter getKafkaTransactionSynchronizationAdapter() {
+ *                 return context.getBean(KafkaTransactionSynchronizationAdapter.class);
+ *             }
+ *         };
+ *     }
+ * **/
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
