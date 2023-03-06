000b69df3e43b16bafa4b4c359d94cd45136afb5
aroze <aroze@vodafone.ua>
2023-03-06 11:22:35 +0200

    feature[ignore-typeKeys]: add possibility ignore typeKeys for timeline event

--- xm-commons-timeline/src/main/java/com/icthh/xm/commons/timeline/config/TimelineInterceptor.java
+++ xm-commons-timeline/src/main/java/com/icthh/xm/commons/timeline/config/TimelineInterceptor.java
@@ -1,5 +1,6 @@
 package com.icthh.xm.commons.timeline.config;
 
+import static java.util.Arrays.asList;
 import static org.apache.commons.collections.CollectionUtils.isNotEmpty;
 import static org.apache.commons.lang3.ObjectUtils.firstNonNull;
 
@@ -9,6 +10,9 @@ import java.util.List;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
+
+import com.jayway.jsonpath.JsonPath;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
@@ -18,6 +22,7 @@ import org.springframework.security.oauth2.provider.authentication.OAuth2Authent
 import org.springframework.stereotype.Component;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.util.ContentCachingResponseWrapper;
 
 @Slf4j
 @Component
@@ -26,27 +31,31 @@ public class TimelineInterceptor extends HandlerInterceptorAdapter {
     private static final String HEADER_TENANT = "x-tenant";
     private static final String AUTH_TENANT_KEY = "tenant";
     private static final String AUTH_USER_KEY = "user_key";
+    private static final String TYPE_KEY = "typeKey";
 
     private final AntPathMatcher matcher = new AntPathMatcher();
 
     private final TimelineEventProducer eventProducer;
     private final List<String> ignoredPatterns;
     private final List<String> ignoredHttpMethods;
+    private final List<String> ignoredTypeKeys;
 
     public TimelineInterceptor(
         TimelineEventProducer eventProducer,
         @Value("${application.tenant-ignored-path-list:true}") List<String> ignoredPatterns,
-        @Value("${application.timeline-ignored-http-methods:#{T(java.util.Collections).emptyList()}}") List<String> ignoredHttpMethods
-    ) {
+        @Value("${application.timeline-ignored-http-methods:#{T(java.util.Collections).emptyList()}}") List<String> ignoredHttpMethods,
+        @Value("${application.timeline-ignored-typekeys:#{T(java.util.Collections).emptyList()}}") List<String> ignoredTypeKeys
+        ) {
         this.eventProducer = eventProducer;
         this.ignoredPatterns = ignoredPatterns;
         this.ignoredHttpMethods = ignoredHttpMethods;
+        this.ignoredTypeKeys = ignoredTypeKeys;
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
 
-        if (isIgnoredRequest(request)) {
+        if (isIgnoredRequest(request, response)) {
             return;
         }
 
@@ -82,7 +91,7 @@ public class TimelineInterceptor extends HandlerInterceptorAdapter {
         return null;
     }
 
-    private boolean isIgnoredRequest(HttpServletRequest request) {
+    private boolean isIgnoredRequest(HttpServletRequest request, HttpServletResponse response) {
         String path = request.getServletPath();
         String httpMethod = request.getMethod();
 
@@ -97,7 +106,38 @@ public class TimelineInterceptor extends HandlerInterceptorAdapter {
                 }
             }
         }
-        return false;
+
+        String responseBody = getResponseContent(response);
+        String typeKey = getEntityField(responseBody, TYPE_KEY);
+
+        return isNotEmpty(ignoredTypeKeys) && ignoredTypeKeys.contains(typeKey);
+    }
+
+    private String getResponseContent(HttpServletResponse response) {
+        if (response instanceof ContentCachingResponseWrapper) {
+            return new String(((ContentCachingResponseWrapper) response).getContentAsByteArray());
+        }
+        if (response instanceof HttpServletResponseWrapper
+            && ((HttpServletResponseWrapper) response).getResponse() instanceof ContentCachingResponseWrapper) {
+            return new String(((ContentCachingResponseWrapper) ((HttpServletResponseWrapper) response)
+                .getResponse()).getContentAsByteArray());
+        }
+        log.warn("Empty response content because of unsupported response class {}", response.getClass());
+        return "";
+    }
+
+    public String getEntityField(String entity, String field) {
+        List<String> prefixes = asList("$.", "$.xmEntity.");
+
+        for (String prefix: prefixes) {
+            try {
+                return JsonPath.read(entity, prefix + field);
+            } catch (Exception ex) {
+                log.trace("JsonPath exception", ex);
+            }
+        }
+
+        return "";
     }
 
     private void produceTimeline(
