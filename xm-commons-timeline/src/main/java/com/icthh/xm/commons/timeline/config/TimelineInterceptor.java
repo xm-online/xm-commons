425143a12c5ddc4332e2216d1bbff5d6381450c5
aroze <aroze@vodafone.ua>
2023-03-06 11:22:35 +0200

    feature[ignored-typeKeys]: add possibility ignore typeKeys for timeline event

--- xm-commons-timeline/src/main/java/com/icthh/xm/commons/timeline/config/TimelineInterceptor.java
+++ xm-commons-timeline/src/main/java/com/icthh/xm/commons/timeline/config/TimelineInterceptor.java
@@ -1,17 +1,21 @@
 package com.icthh.xm.commons.timeline.config;
 
+import static java.util.Arrays.asList;
 import com.icthh.xm.commons.security.internal.XmAuthentication;
 import com.icthh.xm.commons.timeline.TimelineEventProducer;
 import lombok.extern.slf4j.Slf4j;
+import com.jayway.jsonpath.JsonPath;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -26,27 +30,31 @@ public class TimelineInterceptor extends HandlerInterceptorAdapter {
     private static final String HEADER_TENANT = "x-tenant";
     private static final String AUTH_TENANT_KEY = "tenant";
     private static final String AUTH_USER_KEY = "user_key";
+    private static final String TYPE_KEY = "typeKey";
 
     private final AntPathMatcher matcher = new AntPathMatcher();
 
     private final TimelineEventProducer eventProducer;
     private final List<String> ignoredPatterns;
     private final List<String> ignoredHttpMethods;
+    private final List<String> ignoredTypeKeys;
 
     public TimelineInterceptor(
         TimelineEventProducer eventProducer,
         @Value("${application.tenant-ignored-path-list:true}") List<String> ignoredPatterns,
-        @Value("${application.timeline-ignored-http-methods:#{T(java.util.Collections).emptyList()}}") List<String> ignoredHttpMethods
-    ) {
+        @Value("${application.timeline-ignored-http-methods:#{T(java.util.Collections).emptyList()}}") List<String> ignoredHttpMethods,
+        @Value("${application.timeline-ignored-type-keys:#{T(java.util.Collections).emptyList()}}") List<String> ignoredTypeKeys
+        ) {
         this.eventProducer = eventProducer;
         this.ignoredPatterns = ignoredPatterns;
         this.ignoredHttpMethods = ignoredHttpMethods;
+        this.ignoredTypeKeys = ignoredTypeKeys;
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
 
-        if (isIgnoredRequest(request)) {
+        if (isIgnoredRequest(request, response)) {
             return;
         }
 
@@ -82,7 +90,7 @@ public class TimelineInterceptor extends HandlerInterceptorAdapter {
         return null;
     }
 
-    private boolean isIgnoredRequest(HttpServletRequest request) {
+    private boolean isIgnoredRequest(HttpServletRequest request, HttpServletResponse response) {
         String path = request.getServletPath();
         String httpMethod = request.getMethod();
 
@@ -97,7 +105,37 @@ public class TimelineInterceptor extends HandlerInterceptorAdapter {
                 }
             }
         }
-        return false;
+
+        String responseBody = getResponseContent(response);
+        String typeKey = getEntityField(responseBody, TYPE_KEY);
+
+        return isNotEmpty(ignoredTypeKeys) && ignoredTypeKeys.contains(typeKey);
+    }
+
+    private String getResponseContent(HttpServletResponse response) {
+        if (response instanceof ContentCachingResponseWrapper cachingResponseWrapper) {
+            return new String(cachingResponseWrapper.getContentAsByteArray());
+        }
+        if (response instanceof HttpServletResponseWrapper httpServletResponseWrapper
+            && httpServletResponseWrapper.getResponse() instanceof ContentCachingResponseWrapper contentCachingResponseWrapper) {
+            return new String(contentCachingResponseWrapper.getContentAsByteArray());
+        }
+        log.warn("Empty response content because of unsupported response class {}", response.getClass());
+        return "";
+    }
+
+    public String getEntityField(String entity, String field) {
+        List<String> prefixes = asList("$.", "$.xmEntity.");
+
+        for (String prefix: prefixes) {
+            try {
+                return JsonPath.read(entity, prefix + field);
+            } catch (Exception ex) {
+                log.trace("JsonPath exception", ex);
+            }
+        }
+
+        return "";
     }
 
     private void produceTimeline(
--- xm-commons-timeline/src/test/java/com/icthh/xm/commons/timeline/config/TimelineInterceptorUnitTest.java
+++ xm-commons-timeline/src/test/java/com/icthh/xm/commons/timeline/config/TimelineInterceptorUnitTest.java
@@ -1,20 +1,26 @@
 package com.icthh.xm.commons.timeline.config;
 
+import com.icthh.xm.commons.timeline.TimelineEventProducer;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.List;
+
 import static java.util.Arrays.asList;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import com.icthh.xm.commons.timeline.TimelineEventProducer;
-import java.util.Collections;
-import javax.servlet.http.HttpServletRequest;
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-
 public class TimelineInterceptorUnitTest {
+
+    private static final String RESPONSE_TYPE_KEY_LEAD = "{\"typeKey\": \"LEAD\"}";
+    private static final String RESPONSE_TYPE_KEY_FAKE_LEAD = "{\"typeKey\": \"FAKE-LEAD\"}";
 
     private TimelineInterceptor timelineInterceptor;
 
@@ -22,12 +28,15 @@ public class TimelineInterceptorUnitTest {
     private HttpServletRequest httpServletRequest;
 
     @Mock
+    ContentCachingResponseWrapper contentCachingResponseWrapper;
+
+    @Mock
     private TimelineEventProducer eventProducer;
 
     @Before
     public void before() {
         MockitoAnnotations.initMocks(this);
-        timelineInterceptor = new TimelineInterceptor(eventProducer, Collections.emptyList(), asList("GET", "POST"));
+        timelineInterceptor = new TimelineInterceptor(eventProducer, Collections.emptyList(), asList("GET", "POST"), List.of("LEAD"));
     }
 
     @Test
@@ -51,7 +60,29 @@ public class TimelineInterceptorUnitTest {
     @Test
     public void shouldRunLogicForPUTMethod() {
         when(httpServletRequest.getMethod()).thenReturn("PUT");
-        timelineInterceptor.afterCompletion(httpServletRequest, null, null, null);
+        byte[] bytes = RESPONSE_TYPE_KEY_FAKE_LEAD.getBytes();
+        when(contentCachingResponseWrapper.getContentAsByteArray()).thenReturn(bytes);
+        timelineInterceptor.afterCompletion(httpServletRequest, contentCachingResponseWrapper, null, null);
+
+        verify(eventProducer, times(1)).createEventJson(any(), any(), any(), any(), any());
+        verify(eventProducer, times(1)).send(any(), any());
+    }
+
+    @Test
+    public void shouldNotRunLogicForLEADTypeKey() {
+        byte[] bytes = RESPONSE_TYPE_KEY_LEAD.getBytes();
+        when(contentCachingResponseWrapper.getContentAsByteArray()).thenReturn(bytes);
+        timelineInterceptor.afterCompletion(httpServletRequest, contentCachingResponseWrapper, null, null);
+
+        verify(eventProducer, times(0)).createEventJson(any(), any(), any(), any(), any());
+        verify(eventProducer, times(0)).send(any(), any());
+    }
+
+    @Test
+    public void shouldRunLogicForFakeTypeKey() {
+        byte[] bytes = RESPONSE_TYPE_KEY_FAKE_LEAD.getBytes();
+        when(contentCachingResponseWrapper.getContentAsByteArray()).thenReturn(bytes);
+        timelineInterceptor.afterCompletion(httpServletRequest, contentCachingResponseWrapper, null, null);
 
         verify(eventProducer, times(1)).createEventJson(any(), any(), any(), any(), any());
         verify(eventProducer, times(1)).send(any(), any());
