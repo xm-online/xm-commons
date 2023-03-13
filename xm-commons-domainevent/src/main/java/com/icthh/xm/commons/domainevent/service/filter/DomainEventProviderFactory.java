012099148bc25272f4f8949a5d394d93c4e4327e
emurenko <murenko-ye@mc.gcf>
2023-03-13 14:46:27 +0200

    returned ConditionalOnProperty in WebApiSource and DomainEventProviderFactory

--- xm-commons-domainevent/src/main/java/com/icthh/xm/commons/domainevent/service/filter/DomainEventProviderFactory.java
+++ xm-commons-domainevent/src/main/java/com/icthh/xm/commons/domainevent/service/filter/DomainEventProviderFactory.java
@@ -12,6 +12,7 @@ import java.util.List;
 
 @Component
 @RequiredArgsConstructor
+@ConditionalOnProperty(value = "application.domain-event.enabled", havingValue = "true")
 public class DomainEventProviderFactory {
 
     @Value("${spring.application.name}")
