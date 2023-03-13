64be0b98cb43f15b42635a8097de3eba0390c2bc
vitalii.kyrychenko <vitalii.kyrychenko@jevera.software>
2023-03-13 12:14:46 +0100

    fix issue with parameter order

--- xm-commons-domainevent-db/src/main/java/com/icthh/xm/commons/domainevent/db/service/DatabaseFilter.java
+++ xm-commons-domainevent-db/src/main/java/com/icthh/xm/commons/domainevent/db/service/DatabaseFilter.java
@@ -1,7 +1,7 @@
 package com.icthh.xm.commons.domainevent.db.service;
 
 import com.icthh.xm.commons.domainevent.db.domain.JpaEntityContext;
-import com.icthh.xm.commons.domainevent.db.lep.FilterKeyResolver;
+import com.icthh.xm.commons.domainevent.keyresolver.DomainEventFilterKeyResolver;
 import com.icthh.xm.commons.lep.LogicExtensionPoint;
 import com.icthh.xm.commons.lep.spring.LepService;
 import org.springframework.stereotype.Component;
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 @LepService(group = "domainevent.db.filter")
 public class DatabaseFilter {
 
-    @LogicExtensionPoint(value = "DatabaseFilter", resolver = FilterKeyResolver.class)
+    @LogicExtensionPoint(value = "DatabaseFilter", resolver = DomainEventFilterKeyResolver.class)
     public Boolean lepFiltering(String key, String tableName, JpaEntityContext context) {
         return null;
     }
