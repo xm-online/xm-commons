f4f99735eafa84fd3182638837b2742c3a146978
Yurii Matora <ymatora@itsmartflex.com>
2023-03-14 10:31:08 +0100

    Add message not readable exception handler

--- xm-commons-exceptions/src/main/java/com/icthh/xm/commons/exceptions/ErrorConstants.java
+++ xm-commons-exceptions/src/main/java/com/icthh/xm/commons/exceptions/ErrorConstants.java
@@ -6,6 +6,7 @@ public final class ErrorConstants {
     public static final String ERR_ACCESS_DENIED = "error.accessDenied";
     public static final String ERR_VALIDATION = "error.validation";
     public static final String ERR_METHOD_NOT_SUPPORTED = "error.methodNotSupported";
+    public static final String ERR_MESSAGE_NOT_READABLE = "error.messageNotReadable";
     public static final String ERR_INTERNAL_SERVER_ERROR = "error.internalServerError";
     public static final String ERR_BUSINESS = "error.business";
     public static final String ERR_BUSINESS_IDEXISTS = "error.business.idexists";
