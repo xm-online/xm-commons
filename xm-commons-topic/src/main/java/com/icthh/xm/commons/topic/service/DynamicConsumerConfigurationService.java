7c74919005c7fa29d0607bedcf2b890489200408
ichupryna <ichupryna@gmail.com>
2023-03-27 09:59:09 +0300

    Fix spring cycle

--- xm-commons-topic/src/main/java/com/icthh/xm/commons/topic/service/DynamicConsumerConfigurationService.java
+++ xm-commons-topic/src/main/java/com/icthh/xm/commons/topic/service/DynamicConsumerConfigurationService.java
@@ -4,9 +4,10 @@ import com.icthh.xm.commons.config.client.repository.TenantListRepository;
 import com.icthh.xm.commons.topic.domain.DynamicConsumer;
 import com.icthh.xm.commons.topic.domain.TopicConfig;
 import com.icthh.xm.commons.topic.service.dto.RefreshDynamicConsumersEvent;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,12 +16,19 @@ import java.util.stream.Collectors;
 
 @Slf4j
 @Service
-@RequiredArgsConstructor
 public class DynamicConsumerConfigurationService implements ApplicationListener<RefreshDynamicConsumersEvent> {
 
     private final List<DynamicConsumerConfiguration> dynamicConsumerConfigurations;
     private final TopicManagerService topicManagerService;
     private final TenantListRepository tenantListRepository;
+
+    public DynamicConsumerConfigurationService(@Lazy List<DynamicConsumerConfiguration> dynamicConsumerConfigurations,
+                                               @Autowired TopicManagerService topicManagerService,
+                                               @Autowired TenantListRepository tenantListRepository) {
+        this.dynamicConsumerConfigurations = dynamicConsumerConfigurations;
+        this.topicManagerService = topicManagerService;
+        this.tenantListRepository = tenantListRepository;
+    }
 
     public void startDynamicConsumers(String tenantKey) {
         List<DynamicConsumer> dynamicConsumers = getDynamicConsumersByTenant(tenantKey);
