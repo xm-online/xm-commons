39a0f4f80af1a65472784c525113c0cf2f413f97
aroze <aroze@vodafone.ua>
2023-07-04 10:36:13 +0300

    [tryUseCustomOracleDialect] create CustomOracle12cDialect, increment version to 2.2.41oracleDialect

---
+++ xm-commons-migration-db/src/main/java/com/icthh/xm/commons/migration/db/jsonb/CustomOracle12cDialect.java
@@ -0,0 +1,16 @@
+package com.icthh.xm.commons.migration.db.jsonb;
+
+import org.hibernate.dialect.Oracle12cDialect;
+import org.hibernate.dialect.function.SQLFunctionTemplate;
+import org.hibernate.type.StringType;
+
+public class CustomOracle12cDialect extends Oracle12cDialect {
+
+    public static final String JSON_QUERY = "json_query";
+    public static final String JSON_QUERY_TEMPLATE = "json_value(?1, ?2)";
+
+    public CustomOracle12cDialect() {
+        registerFunction(JSON_QUERY, new SQLFunctionTemplate(StringType.INSTANCE, JSON_QUERY_TEMPLATE));
+    }
+
+}
