432776f85ec9b3e8bb916137ce1c09a1b630f6d4
amedved <sirius.nau@gmail.com>
2023-10-10 14:55:39 +0300

    Suppress excessive logging for domain events

--- xm-commons-domainevent-db/src/main/java/com/icthh/xm/commons/domainevent/db/service/DatabaseDslFilter.java
+++ xm-commons-domainevent-db/src/main/java/com/icthh/xm/commons/domainevent/db/service/DatabaseDslFilter.java
@@ -4,12 +4,14 @@ import com.icthh.xm.commons.domainevent.db.domain.JpaEntityContext;
 import com.icthh.xm.commons.domainevent.db.lep.FilterKeyResolver;
 import com.icthh.xm.commons.lep.LogicExtensionPoint;
 import com.icthh.xm.commons.lep.spring.LepService;
+import com.icthh.xm.commons.logging.LoggingAspectConfig;
 import org.springframework.stereotype.Component;
 
 @Component
 @LepService(group = "domainevent.db.filter")
 public class DatabaseDslFilter {
 
+    @LoggingAspectConfig(inputExcludeParams = "context")
     @LogicExtensionPoint(value = "DatabaseDslFilter", resolver = FilterKeyResolver.class)
     public Boolean lepFiltering(String key, String tableName, JpaEntityContext context) {
         return null;
