771f98ddb3e8d20e969dc0d1d19836ff38ca788a
amedved <sirius.nau@gmail.com>
2023-10-10 14:33:10 +0300

    suppress excessive logging related to domain events

--- xm-commons-domainevent/src/main/java/com/icthh/xm/commons/domainevent/service/EventPublisher.java
+++ xm-commons-domainevent/src/main/java/com/icthh/xm/commons/domainevent/service/EventPublisher.java
@@ -4,6 +4,7 @@ import com.icthh.xm.commons.domainevent.config.XmDomainEventConfiguration;
 import com.icthh.xm.commons.domainevent.domain.DomainEvent;
 import com.icthh.xm.commons.lep.LogicExtensionPoint;
 import com.icthh.xm.commons.lep.spring.LepService;
+import com.icthh.xm.commons.logging.LoggingAspectConfig;
 import com.icthh.xm.commons.tenant.TenantContextHolder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,6 +19,7 @@ public class EventPublisher {
     private final XmDomainEventConfiguration xmDomainEventConfiguration;
     private final TenantContextHolder tenantContextHolder;
 
+    @LoggingAspectConfig(inputExcludeParams = "event")
     @LogicExtensionPoint(value = "Publish")
     public void publish(String source, DomainEvent event) {
         event.setSource(source);
