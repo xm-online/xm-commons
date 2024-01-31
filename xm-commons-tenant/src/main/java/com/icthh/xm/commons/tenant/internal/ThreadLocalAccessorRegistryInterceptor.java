0d6a5162a22ef5e8b73ca3a55996d6c9ba6900b0
valentyna.osiievska <valentyna.osiievska@dia.com.ua>
2024-01-31 09:49:04 +0100

    created ThreadLocalAccessorRegistryInterceptor

---
+++ xm-commons-tenant/src/main/java/com/icthh/xm/commons/tenant/internal/ThreadLocalAccessorRegistryInterceptor.java
@@ -0,0 +1,32 @@
+package com.icthh.xm.commons.tenant.internal;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.lang.reflect.Method;
+
+@Slf4j
+public class ThreadLocalAccessorRegistryInterceptor {
+
+    /**
+     * This method registers ThreadLocalAccessor in micrometer ContextRegistry. Ignored if micrometer is missing.
+     *
+     * @param key the key to associate with the ThreadLocal value
+     * @param threadLocal the underlying ThreadLocal
+     */
+    static void register(String key, ThreadLocal<TenantContextDataHolder> threadLocal) {
+        try {
+            Class<?> contextRegistry = Class.forName("io.micrometer.context.ContextRegistry");
+            Method getInstance = contextRegistry.getMethod("getInstance");
+            Method registerThreadLocalAccessor = contextRegistry.getMethod("registerThreadLocalAccessor", String.class, ThreadLocal.class);
+
+            Object instance = getInstance.invoke(null);
+            registerThreadLocalAccessor.invoke(instance, key, threadLocal);
+            log.info("Successfully registered ThreadLocal in micrometer ContextRegistry by key '{}'", key);
+
+        } catch (ClassNotFoundException | NoSuchMethodException | LinkageError ignored) {
+        } catch (Throwable err) {
+            log.error("Unexpected exception while registering ThreadLocal in micrometer ContextRegistry. " +
+                "The feature is considered disabled due to this:", err);
+        }
+    }
+}
