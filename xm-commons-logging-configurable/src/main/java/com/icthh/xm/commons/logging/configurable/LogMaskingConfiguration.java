1413afb70bb5b52e0a40d53a603ee32308cf2f11
Gitlab CI/CD <gitlab@jevera.software>
2024-04-05 16:07:50 +0300

    Add masking layout

---
+++ xm-commons-logging-configurable/src/main/java/com/icthh/xm/commons/logging/configurable/LogMaskingConfiguration.java
@@ -0,0 +1,47 @@
+package com.icthh.xm.commons.logging.configurable;
+
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.Layout;
+import ch.qos.logback.core.OutputStreamAppender;
+import ch.qos.logback.core.encoder.Encoder;
+import ch.qos.logback.core.encoder.LayoutWrappingEncoder;
+import com.icthh.xm.commons.logging.config.LoggingConfigService;
+import com.icthh.xm.commons.logging.util.MaskingLayout;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+
+import static org.springframework.core.Ordered.HIGHEST_PRECEDENCE;
+
+@Slf4j
+@Configuration
+@Order(HIGHEST_PRECEDENCE)
+public class LogMaskingConfiguration {
+
+    public LogMaskingConfiguration(LoggingConfigService loggingConfigService) {
+        log.info("Init MaskingPatternLayout");
+        LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
+        context.getLogger("ROOT").iteratorForAppenders().forEachRemaining(it -> {
+            if (it instanceof OutputStreamAppender) {
+                var appender = (OutputStreamAppender<ILoggingEvent>) it;
+                Encoder<ILoggingEvent> encoder = appender.getEncoder();
+                if (encoder instanceof LayoutWrappingEncoder) {
+                    LayoutWrappingEncoder<ILoggingEvent> layoutEncoder = (LayoutWrappingEncoder<ILoggingEvent>) encoder;
+                    Layout<ILoggingEvent> layout = layoutEncoder.getLayout();
+
+                    LayoutWrappingEncoder<ILoggingEvent> maskingEncoder = new LayoutWrappingEncoder<>();
+                    maskingEncoder.setLayout(new MaskingLayout(layout, loggingConfigService));
+                    maskingEncoder.setContext(context);
+                    maskingEncoder.setCharset(layoutEncoder.getCharset());
+                    maskingEncoder.start();
+
+                    appender.setEncoder(maskingEncoder);
+                }
+            }
+        });
+    }
+
+}
--- xm-commons-logging-configurable/src/main/java/com/icthh/xm/commons/logging/configurable/LoggingRefreshableConfiguration.java
+++ xm-commons-logging-configurable/src/main/java/com/icthh/xm/commons/logging/configurable/LoggingRefreshableConfiguration.java
@@ -6,7 +6,9 @@ import com.icthh.xm.commons.config.client.api.RefreshableConfiguration;
 import com.icthh.xm.commons.logging.config.LoggingConfig;
 import com.icthh.xm.commons.logging.config.LoggingConfig.LepLogConfiguration;
 import com.icthh.xm.commons.logging.config.LoggingConfig.LogConfiguration;
+import com.icthh.xm.commons.logging.config.LoggingConfig.MaskingLogConfiguration;
 import com.icthh.xm.commons.logging.config.LoggingConfigService;
+import com.icthh.xm.commons.logging.util.MaskingService;
 import com.icthh.xm.commons.tenant.TenantContextHolder;
 import com.icthh.xm.commons.tenant.TenantKey;
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +19,7 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import org.springframework.util.AntPathMatcher;
 
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -27,9 +30,12 @@ import static com.icthh.xm.commons.tenant.TenantContextUtils.getTenantKey;
 @Primary
 public class LoggingRefreshableConfiguration implements RefreshableConfiguration, LoggingConfigService {
 
+    private final MaskingService NULL_MASKING_SERVICE;
+
     private final Map<String, Map<String, LogConfiguration>> serviceLoggingConfig = new ConcurrentHashMap<>();
     private final Map<String, Map<String, LogConfiguration>> apiLoggingConfig = new ConcurrentHashMap<>();
     private final Map<String, Map<String, LepLogConfiguration>> lepLoggingConfig = new ConcurrentHashMap<>();
+    private final Map<String, MaskingService> maskingConfig = new ConcurrentHashMap<>();
 
     private final AntPathMatcher matcher = new AntPathMatcher();
     private final ObjectMapper ymlMapper = new ObjectMapper(new YAMLFactory());
@@ -37,12 +43,19 @@ public class LoggingRefreshableConfiguration implements RefreshableConfiguration
     private final TenantContextHolder tenantContextHolder;
     private final String mappingPath;
     private final String appName;
+    private final List<String> maskPatterns;
 
     public LoggingRefreshableConfiguration(TenantContextHolder tenantContextHolder,
-                                           @Value("${spring.application.name}") String appName) {
+                                           @Value("${spring.application.name}") String appName,
+                                           @Value("${application.maskPatterns:#{T(java.util.Collections).emptyList()}}")
+                                           List<String> maskPatterns) {
         this.tenantContextHolder = tenantContextHolder;
         this.mappingPath = "/config/tenants/{tenantName}/" + appName + "/logging.yml";
         this.appName = appName;
+        this.maskPatterns = maskPatterns;
+        MaskingLogConfiguration maskingLogConfiguration = new MaskingLogConfiguration();
+        maskingLogConfiguration.setEnabled(!maskPatterns.isEmpty());
+        this.NULL_MASKING_SERVICE = new MaskingService(maskingLogConfiguration, maskPatterns);
     }
 
     @Override
@@ -60,6 +73,7 @@ public class LoggingRefreshableConfiguration implements RefreshableConfiguration
             this.serviceLoggingConfig.put(tenant, spec.buildServiceLoggingConfigs());
             this.apiLoggingConfig.put(tenant, spec.buildApiLoggingConfigs());
             this.lepLoggingConfig.put(tenant, spec.buildLepLoggingConfigs(tenant, appName));
+            this.maskingConfig.put(tenant, new MaskingService(spec.getMasking(), maskPatterns));
 
             log.info("Tenant configuration was updated for tenant [{}] by key [{}]", tenant, updatedKey);
         } catch (Exception e) {
@@ -77,6 +91,14 @@ public class LoggingRefreshableConfiguration implements RefreshableConfiguration
         if (this.isListeningConfiguration(configKey)) {
             this.onRefresh(configKey, configValue);
         }
+    }
+
+    @Override
+    public MaskingService getMaskingService() {
+        return getTenantKey(this.tenantContextHolder)
+            .map(TenantKey::getValue)
+            .map(maskingConfig::get)
+            .orElse(NULL_MASKING_SERVICE);
     }
 
     @Override
