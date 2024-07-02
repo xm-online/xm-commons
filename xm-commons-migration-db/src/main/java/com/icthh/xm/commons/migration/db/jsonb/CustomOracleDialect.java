9c872ed1d30ff137dd929991c990152b85097f0d
vosiievska <valentinaosievskaya@gmail.com>
2024-07-02 11:13:51 +0300

    VF-39 updated json_value function registration for CustomOracleDialect

--- xm-commons-migration-db/src/main/java/com/icthh/xm/commons/migration/db/jsonb/CustomOracleDialect.java
+++ xm-commons-migration-db/src/main/java/com/icthh/xm/commons/migration/db/jsonb/CustomOracleDialect.java
@@ -2,21 +2,23 @@ package com.icthh.xm.commons.migration.db.jsonb;
 
 import org.hibernate.boot.model.FunctionContributions;
 import org.hibernate.dialect.OracleDialect;
-import org.hibernate.query.sqm.function.SqmFunctionRegistry;
 import org.hibernate.type.BasicType;
 import org.hibernate.type.StandardBasicTypes;
 
-// todo spring 3.2.0 migration (https://docs.jboss.org/hibernate/orm/6.0/migration-guide/migration-guide.html#_dialects)
 public class CustomOracleDialect extends OracleDialect implements CustomDialect {
 
     public static final String JSON_QUERY_TEMPLATE = "json_value(?1, ?2)";
 
-    public CustomOracleDialect(FunctionContributions functionContributions) {
-        BasicType<String> stringBasicType = functionContributions.getTypeConfiguration()
-            .getBasicTypeRegistry().resolve(StandardBasicTypes.STRING);
-        SqmFunctionRegistry functionRegistry = functionContributions.getFunctionRegistry();
+    @Override
+    public void initializeFunctionRegistry(FunctionContributions functionContributions) {
+        super.initializeFunctionRegistry(functionContributions);
 
-        functionRegistry.registerPattern(JSON_QUERY, JSON_QUERY_TEMPLATE, stringBasicType);
+        BasicType<String> stringBasicType = functionContributions
+            .getTypeConfiguration()
+            .getBasicTypeRegistry()
+            .resolve(StandardBasicTypes.STRING);
+
+        functionContributions.getFunctionRegistry().registerPattern(JSON_QUERY, JSON_QUERY_TEMPLATE, stringBasicType);
     }
 
 }
