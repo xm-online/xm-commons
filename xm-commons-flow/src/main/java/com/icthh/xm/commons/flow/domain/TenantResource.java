4ddf18333aaffa49a88b98c5f5b7cc4f8e781cb5
ssenko <gitlab@jevera.software>
2024-07-17 11:37:29 +0300

    fix tenant resource npe

--- xm-commons-flow/src/main/java/com/icthh/xm/commons/flow/domain/TenantResource.java
+++ xm-commons-flow/src/main/java/com/icthh/xm/commons/flow/domain/TenantResource.java
@@ -4,6 +4,7 @@ import com.icthh.xm.commons.config.client.api.refreshable.ConfigWithKey;
 import lombok.Data;
 
 import java.time.Instant;
+import java.util.HashMap;
 import java.util.Map;
 
 @Data
@@ -13,4 +14,25 @@ public class TenantResource implements ConfigWithKey {
     private Map<String, String> name;
     private Map<String, Object> data;
     private Instant updateDate;
+
+    public String getKey() {
+        if (key == null) {
+            return "";
+        }
+        return key;
+    }
+
+    public String getResourceType() {
+        if (resourceType == null) {
+            return "";
+        }
+        return resourceType;
+    }
+
+    public Map<String, Object> getData() {
+        if (data == null) {
+            data = new HashMap<>();
+        }
+        return data;
+    }
 }
