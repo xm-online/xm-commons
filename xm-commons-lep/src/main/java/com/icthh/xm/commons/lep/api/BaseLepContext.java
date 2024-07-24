c6ed09588033ffa4affd7e5d12496e70727694b9
ssenko <gitlab@jevera.software>
2024-07-24 12:55:37 +0300

    Fix base lep context master

--- xm-commons-lep/src/main/java/com/icthh/xm/commons/lep/api/BaseLepContext.java
+++ xm-commons-lep/src/main/java/com/icthh/xm/commons/lep/api/BaseLepContext.java
@@ -4,6 +4,7 @@ import com.icthh.xm.commons.lep.BaseProceedingLep;
 import com.icthh.xm.commons.lep.spring.LepThreadHelper;
 import com.icthh.xm.commons.lep.spring.lepservice.LepServiceFactory;
 import com.icthh.xm.commons.security.XmAuthenticationContext;
+import com.icthh.xm.commons.security.XmAuthenticationContextHolder;
 import com.icthh.xm.commons.tenant.TenantContext;
 
 import java.util.HashMap;
@@ -16,6 +17,7 @@ public abstract class BaseLepContext {
     public BaseProceedingLep lep;
     public LepThreadHelper thread;
     public XmAuthenticationContext authContext;
+    public XmAuthenticationContextHolder authContextHolder;
     public TenantContext tenantContext;
     // just remove usage, no alternatives
     @Deprecated(forRemoval = true)
@@ -26,6 +28,14 @@ public abstract class BaseLepContext {
 
     public Object get(Object additionalContextKey) {
         return additionalContext.get(additionalContextKey);
+    }
+
+    public Object get(String additionalContextKey) {
+        return additionalContext.get(additionalContextKey);
+    }
+
+    public Object propertyMissing(String prop) {
+        return get(prop);
     }
 
     public final void addAdditionalContext(String additionalContextKey, Object additionalContextValue) {
@@ -40,4 +50,8 @@ public abstract class BaseLepContext {
         lepContext.additionalContext = additionalContext;
     }
 
+    @Override
+    public String toString() {
+        return "lepContext[hidden]";
+    }
 }
