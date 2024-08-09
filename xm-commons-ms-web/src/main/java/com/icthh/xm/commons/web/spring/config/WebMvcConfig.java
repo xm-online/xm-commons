273b46dcc0b3420fb4e257e80550360026a571e4
vosiievska <valentinaosievskaya@gmail.com>
2024-08-09 16:44:19 +0300

    VF-43 removed jackson-datatype-hibernate5

--- xm-commons-ms-web/src/main/java/com/icthh/xm/commons/web/spring/config/WebMvcConfig.java
+++ xm-commons-ms-web/src/main/java/com/icthh/xm/commons/web/spring/config/WebMvcConfig.java
@@ -4,7 +4,6 @@ import com.icthh.xm.commons.tenant.XmRelatedComponent;
 import com.icthh.xm.commons.web.spring.TenantInterceptor;
 import com.icthh.xm.commons.web.spring.XmLoggingInterceptor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.AsyncHandlerInterceptor;
@@ -17,7 +16,6 @@ import java.util.List;
 @Import({
     XmMsWebConfiguration.class
 })
-@ConditionalOnMissingBean(XmWebMvcConfigurerAdapter.class)
 public class WebMvcConfig extends XmWebMvcConfigurerAdapter {
 
     private final List<AsyncHandlerInterceptor> interceptors;
