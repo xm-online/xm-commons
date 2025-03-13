28cb6008f276f4fa5d74015b5f9ebdc738b5ba5e
vosiievska <valentinaosievskaya@gmail.com>
2025-03-13 17:31:03 +0200

    added new functions to jsonb expression

--- xm-commons-migration-db/src/main/java/com/icthh/xm/commons/migration/db/jsonb/JsonbExpression.java
+++ xm-commons-migration-db/src/main/java/com/icthh/xm/commons/migration/db/jsonb/JsonbExpression.java
@@ -7,12 +7,15 @@ import org.hibernate.type.descriptor.jdbc.JsonJdbcType;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.function.Function;
+import java.util.stream.Stream;
 
 import static com.icthh.xm.commons.migration.db.jsonb.CustomDialect.JSON_QUERY;
 import static com.icthh.xm.commons.migration.db.jsonb.CustomPostgreSQLDialect.JSON_EXTRACT_PATH;
+import static com.icthh.xm.commons.migration.db.jsonb.CustomPostgreSQLDialect.JSON_EXTRACT_PATH_TEMPLATE_SIMPLE;
 import static com.icthh.xm.commons.migration.db.jsonb.CustomPostgreSQLDialect.TO_JSON_B;
 import static com.icthh.xm.commons.migration.db.jsonb.CustomPostgreSQLDialect.TO_JSON_B_TEXT;
 
@@ -76,8 +79,14 @@ public class JsonbExpression implements CustomExpression {
         return cb.function(TO_JSON_B_TEXT, type, expression);
     }
 
-    public Expression<String> jsonbToString(CriteriaBuilder cb, Root<?> root, String column, String jsonPath) {
-        return cb.function(JSON_EXTRACT_PATH, String.class, root.get(column), cb.literal(jsonPath));
+    public Expression<String> jsonbToString(CriteriaBuilder cb, Root<?> root, String column, String... jsonPath) {
+        Expression[] params = Stream.concat(
+                Stream.of(root.get(column)),
+                Arrays.stream(jsonPath).map(cb::literal)
+            )
+            .toArray(Expression[]::new);
+
+        return cb.function(JSON_EXTRACT_PATH_TEMPLATE_SIMPLE, String.class, params);
     }
 
     public List<? extends Expression<?>> toJsonbCollection(Collection<?> collection, Function<Object, Expression<?>> converter) {
