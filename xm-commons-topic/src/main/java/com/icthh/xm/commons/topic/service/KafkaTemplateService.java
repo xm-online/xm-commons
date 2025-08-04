866cb47e347a370d904968c5624c617ccbbd94ec
osiievska23 <valentinaosievskaya@gmail.com>
2025-08-04 10:29:54 +0300

    kafka template service - send message with headers

--- xm-commons-topic/src/main/java/com/icthh/xm/commons/topic/service/KafkaTemplateService.java
+++ xm-commons-topic/src/main/java/com/icthh/xm/commons/topic/service/KafkaTemplateService.java
@@ -3,8 +3,13 @@ package com.icthh.xm.commons.topic.service;
 import com.icthh.xm.commons.logging.LoggingAspectConfig;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.KafkaHeaders;
 import org.springframework.kafka.support.SendResult;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
 @Service
@@ -41,4 +46,52 @@ public class KafkaTemplateService {
                                                              String data) {
         return kafkaTemplate.send(topic, partition, key, data);
     }
+
+    /**
+     * Send the data to the provided topic with headers, key and partition.
+     *
+     * @param topic     the topic.
+     * @param data      The data.
+     * @param headers   The headers that will be included in the record
+     * @return a Future for the {@link SendResult}.
+     **/
+    @LoggingAspectConfig(inputExcludeParams = "data", resultDetails = false)
+    public CompletableFuture<SendResult<String, String>> send(String topic,
+                                                              String data,
+                                                              Map<String, Object> headers) {
+        return send(topic, null, null, data, headers);
+    }
+
+    /**
+     * Send the data to the provided topic with headers and missing key or partition.
+     *
+     * @param topic     the topic.
+     * @param partition the partition.
+     * @param key       the key.
+     * @param data      The data.
+     * @param headers   The headers that will be included in the record
+     * @return a Future for the {@link SendResult}.
+     **/
+    public CompletableFuture<SendResult<String, String>> send(String topic,
+                                                              Integer partition,
+                                                              String key,
+                                                              String data,
+                                                              Map<String, Object> headers) {
+        MessageBuilder<String> builder = MessageBuilder
+            .withPayload(data)
+            .setHeader(KafkaHeaders.TOPIC, topic)
+            .setHeader(KafkaHeaders.KEY, key);
+
+        if (partition != null) {
+            builder.setHeader(KafkaHeaders.PARTITION, partition);
+        }
+
+        if (headers != null) {
+            builder.copyHeaders(headers);
+        }
+
+        Message<String> message = builder.build();
+        return kafkaTemplate.send(message);
+    }
+
 }
