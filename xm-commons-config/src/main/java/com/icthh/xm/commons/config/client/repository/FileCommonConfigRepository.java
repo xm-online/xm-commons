412257afc785843e63b14ba828ed916553beac50
viktorpavlenko <vitek.pavfurious@gmail.com>
2025-10-09 12:26:11 +0300

    Impl fetch file config by pattern paths;

--- xm-commons-config/src/main/java/com/icthh/xm/commons/config/client/repository/FileCommonConfigRepository.java
+++ xm-commons-config/src/main/java/com/icthh/xm/commons/config/client/repository/FileCommonConfigRepository.java
@@ -2,15 +2,19 @@ package com.icthh.xm.commons.config.client.repository;
 
 import com.icthh.xm.commons.config.client.config.XmConfigProperties;
 import com.icthh.xm.commons.config.domain.Configuration;
+import com.icthh.xm.commons.exceptions.BusinessException;
 import java.io.File;
+import java.io.IOException;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import org.apache.commons.io.filefilter.IOFileFilter;
 import org.apache.commons.io.filefilter.TrueFileFilter;
+import org.springframework.util.AntPathMatcher;
 
 import static java.io.File.separator;
 import static java.nio.charset.StandardCharsets.UTF_8;
@@ -25,6 +29,7 @@ public class FileCommonConfigRepository implements CommonConfigRepository {
     private static final IOFileFilter TRUE_FILTER = TrueFileFilter.INSTANCE;
 
     private final XmConfigProperties xmConfigProperties;
+    private final AntPathMatcher antPathMatcher = new AntPathMatcher();
 
     @Override
     public Map<String, Configuration> getConfig(String commit) {
@@ -51,13 +56,39 @@ public class FileCommonConfigRepository implements CommonConfigRepository {
     }
 
     @Override
-    public Map<String, Configuration> getConfigByPatternPaths(String version, Collection<String> paths) {
-        return Map.of();
+    public Map<String, Configuration> getConfigByPatternPaths(String version, Collection<String> patterns) {
+        File basePath = new File(xmConfigProperties.getDirectoryBasePath());
+        List<String> fullPatternPaths = patterns.stream()
+                .map(pattern -> basePath.getPath() + pattern)
+                .toList();
+
+        List<String> filteredConfigPaths = listFiles(basePath, TRUE_FILTER, TRUE_FILTER).stream()
+                .map(File::getPath)
+                .filter(path -> matchPath(path, fullPatternPaths))
+                .toList();
+
+        Map<String, Configuration> configurationMap = new HashMap<>();
+
+        for (String fullPath: filteredConfigPaths) {
+            String content = readFile(new File(fullPath));
+            String path = replaceChars(fullPath, separator, "/");
+            configurationMap.put(path, new Configuration(path, content));
+        }
+        return configurationMap;
+    }
+
+    private boolean matchPath(String path, Collection<String> pathsAntPattern) {
+        return pathsAntPattern.stream().anyMatch(it -> antPathMatcher.match(it, path));
     }
 
     @SneakyThrows
     private static String readFile(File basePath, String relativePath) {
         File file = new File(basePath, relativePath);
+        return readFile(file);
+    }
+
+    @SneakyThrows
+    private static String readFile(File file) {
         if (file.exists()) {
             return readFileToString(file, UTF_8);
         } else {
