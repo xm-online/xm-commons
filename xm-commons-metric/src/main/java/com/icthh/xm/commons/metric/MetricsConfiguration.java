6cf8ba2bb4af769c9f3b06b67084873c9c41548a
pzadorovskyi <pzadorovskyi@itsmartflex.com>
2025-12-03 18:13:05 +0200

    moved kafka metrics to separate config file

--- xm-commons-metric/src/main/java/com/icthh/xm/commons/metric/MetricsConfiguration.java
+++ xm-commons-metric/src/main/java/com/icthh/xm/commons/metric/MetricsConfiguration.java
@@ -7,44 +7,21 @@ import com.ryantenney.metrics.spring.config.annotation.MetricsConfigurerAdapter;
 import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.binder.MeterBinder;
 import java.util.List;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.context.annotation.Import;
 
 @Configuration
+@Import({
+    KafkaMetricsConfiguration.class
+})
 @EnableMetrics(proxyTargetClass = true)
 public class MetricsConfiguration extends MetricsConfigurerAdapter {
-
-    private final Logger log = LoggerFactory.getLogger(MetricsConfiguration.class);
-
-    private MeterRegistry meterRegistry;
-
-    private KafkaAdmin kafkaAdmin;
-
-    @Value("${application.kafkaMetric.enabled:false}")
-    private Boolean kafkaMetricEnabled;
-    @Value("${application.kafkaMetric.connectionTimeoutTopic:#{null}}")
-    private Integer connectionTimeoutTopic;
-    @Value("${application.kafkaMetric.metricTopics:#{null}}")
-    private List<String> metricTopics;
-
-    public MetricsConfiguration(MeterRegistry meterRegistry, KafkaAdmin kafkaAdmin) {
-        this.meterRegistry = meterRegistry;
-        this.kafkaAdmin = kafkaAdmin;
-    }
 
     @Bean
     public MeterBinder operatingSystemMetricsBinder() {
         return new OperatingSystemMetrics(getOperatingSystemMXBean());
-    }
-
-    @Bean
-    public MeterBinder kafkaMetricsBinder() {
-        return new KafkaMetrics(kafkaAdmin, connectionTimeoutTopic, metricTopics);
     }
 
     @Bean
