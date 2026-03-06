68c9d0777ce0d185107567befdacc9e565504414
Bohdan Volokhonenko <null>
2026-03-06 11:47:00 +0200

    fixed test

--- xm-commons-function/src/test/java/com/icthh/xm/commons/web/rest/FunctionResourceMvcIntTest.java
+++ xm-commons-function/src/test/java/com/icthh/xm/commons/web/rest/FunctionResourceMvcIntTest.java
@@ -15,7 +15,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -51,7 +50,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
     FunctionUploadResource.class,
     ExceptionTranslator.class
 })
-@TestPropertySource(properties = "application.functions-api-prefix=/api/functions")
 public class FunctionResourceMvcIntTest {
 
     @Autowired
